83d02fee27db9daafcfeabddbc3f030a8b5bb8be
Matthew Thomas <malex@orbiter.me>
2016-09-25 11:20:08 -0700

    Some renaming and implements first unit test Calc_EmptyString_ThrowsArgumentOutOfRangeException.

--- src/Calculator.Core/Calculator.cs
+++ src/Calculator.Core/Calculator.cs
@@ -1,6 +1,22 @@
-﻿namespace Solution
+﻿using System;
+
+namespace CoolCalc.Core
 {
     public class Calculator
     {
+        /// <summary>
+        /// Calculate the input string.
+        /// </summary>
+        /// <param name="input">A mathematical expression to compute.</param>
+        /// <returns>Returns the result of the computation.</returns>
+        public 
+        int 
+        Calc(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+                throw new ArgumentOutOfRangeException();
+
+            throw new System.NotImplementedException();
+        }
     }
 }
--- src/Calculator.Test.Unit/CalculatorTests.cs
+++ src/Calculator.Test.Unit/CalculatorTests.cs
@@ -1,13 +1,47 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace SolutionTests
+namespace CoolCalc.Core.Test.Unit
 {
     [TestClass]
     public class CalculatorTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public 
+        void
+        Calc_EmptyString_ThrowsArgumentOutOfRangeException()
         {
+            // Arrange
+            var calculator        = new Calculator();
+            var input             = string.Empty;
+            var exceptionReceived = false;
+
+            // Act
+            try
+            {
+                calculator.Calc(input);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                exceptionReceived = true;
+            }
+
+            // Assert
+            Assert.IsTrue(exceptionReceived);
+        }
+
+        [TestMethod]
+        public void Calc_AddTwoNumbers_ReturnsResult()
+        {
+            //// Arrange
+            //var calculator = new Calculator();
+            //var input      = "5+5";
+
+            //// Act
+            //var result = calculator.Calc(input);
+
+            //// Assert
+            //Assert.AreEqual(10, result);
         }
     }
 }
