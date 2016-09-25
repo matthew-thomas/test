df085b523a2d69fd7b0c609628e2fcdbc1c5f0d9
Matthew Thomas <malex@orbiter.me>
2016-09-25 12:13:47 -0700

    Got basic implementation in and passing Calc_AddTwoNumbers_ReturnsResult.

--- src/Calculator.Core/Calculator.cs
+++ src/Calculator.Core/Calculator.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Text.RegularExpressions;
 
 namespace CoolCalc.Core
 {
@@ -10,13 +12,87 @@ namespace CoolCalc.Core
         /// <param name="input">A mathematical expression to compute.</param>
         /// <returns>Returns the result of the computation.</returns>
         public 
-        int 
+        double 
         Calc(string input)
         {
             if (string.IsNullOrWhiteSpace(input))
                 throw new ArgumentOutOfRangeException(nameof(input));
 
-            throw new System.NotImplementedException();
+            // Split expression in to numeral/operator parts.
+            var parts = Regex.Split(input, "([-+*/])");
+
+            var orderedParts = new ArrayList();
+
+            // Compute higher precedence operations on first pass
+            for (var i = 0; i < parts.Length - 1; i++)
+            {
+                var currentValue = parts[i];
+
+                double number;
+                var isNumeric = double.TryParse(currentValue, out number);
+
+                if (isNumeric)
+                {
+                    // Check what operator is next
+                    var nextOperator = parts[i + 1];
+                    var nextValue = long.Parse(parts[i + 2]);
+
+                    switch (nextOperator)
+                    {
+                        case "*":
+                            orderedParts.Add(number*nextValue);
+                            break;
+                        case "/":
+                            orderedParts.Add(number/nextValue);
+                            break;
+                        case "+":
+                        case "-":
+                            orderedParts.Add(currentValue);
+                            orderedParts.Add(nextOperator);
+                            orderedParts.Add(nextValue);
+                            break;
+                    }
+
+                    // Skip to next set.
+                    i += 2;
+                }
+                else
+                {
+                    orderedParts.Add(currentValue);
+                }
+            }
+
+            // Compute all
+            var answer = 0D;
+
+            for (var i = 0; i < orderedParts.Count - 1; i++)
+            {
+                var currentValue = orderedParts[i];
+
+                double number;
+                var isNumeric = double.TryParse(currentValue.ToString(), out number);
+
+                if (isNumeric)
+                {
+                    var nextOperator = orderedParts[i + 1].ToString();
+                    var nextValue = double.Parse(orderedParts[i + 2].ToString());
+
+                    switch (nextOperator)
+                    {
+                        case "+":
+                            answer = number + nextValue;
+                            break;
+                        case "-":
+                            answer = number - nextValue;
+                            break;
+                    }
+                }
+
+                // Skip to next set.
+                i += 2;
+            }
+            
+            return answer;
         }
     }
 }
