ecf0599df6aab8ba73d3ee21cb43bbee5f886243
Matthew Thomas <malex@orbiter.me>
2016-09-25 11:12:11 -0700

    Initial commit of project skeleton.

---
+++ src/Calculator.Test.Unit/CalculatorTests.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SolutionTests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
